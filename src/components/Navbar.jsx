c893cd2f24a6d4c2321a985448c45e6c3e510711
G-Linares <gerrylinares@gmail.com>
2022-01-13 12:58:04 -0600

    ready for deployment

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React , {useState, useEffect} from 'react';
-import { Button, Menu, Typography, Avatar, Menu } from 'antd';
+import { Button, Typography, Avatar, Menu } from 'antd';
 import { Link} from 'react-router-dom';
 import { HomeOutlined, MoneyCollectOutlined, BulbOutlined, FundOutlined, MenuOutlined } from '@ant-design/icons';
 import icon from '../images/logo.png';
